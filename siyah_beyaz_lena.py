9b03e989d182ae2b9b8b14873e4440e2632005a5
SabahattinAkal <109273966+SabahattinAkal@users.noreply.github.com>
2022-07-18 11:15:23 +0200

    Add files via upload

---
+++ siyah_beyaz_lena.py
@@ -0,0 +1,31 @@
+import cv2 
+import numpy as np
+
+
+# Görseli okuma ve ekranda gösterme
+
+resim = cv2.imread("lena.png")
+cv2.imshow("Orjinal Resim",resim)
+
+def siyahBeyazYap(resim,esik):
+    en,boy,katman = np.shape(resim) #resmin boyutlarını alıyoruz.
+
+    yeniResim = np.ones((en,boy,katman)) #beyaz bir görsel oluşturuyoruz.
+
+    for i in range(en):
+
+        for j in range(boy):
+
+            if(resim[i,j,0] > esik): #resmin 0.katmanını eşik değer ile karşılaştırıyoruz.
+
+                yeniResim[i,j] = 1
+
+            else:
+
+                yeniResim[i,j]=0
+
+        cv2.imshow("Siyah Beyaz",yeniResim)
+
+siyahBeyazYap(resim,125)
+
+cv2.waitKey()
